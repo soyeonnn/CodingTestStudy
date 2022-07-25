8e2c32d94563e53e2aeb1e265d1fcbc7d4796579
soyeonnn <no02031@naver.com>
2022-07-25 17:14:30 +0900

    백준 시간초과..

--- Baekjoon/그래프와순회/24479.py
+++ Baekjoon/그래프와순회/24479.py
@@ -7,18 +7,18 @@ def dfs(graph, v, visited):
         if not visited[i]:
             dfs(graph, i, visited)
 
-# import sys
-# input = sys.stdin.readline
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 n,m,r = map(int, input().split())
 graph = []
-for i in range(n + 1):
-    graph.append([])
+graph = [[] for _ in range(n+1)]
 
 visited = [False] * (n+1)
 visit_order = []
 
 for i in range(m):
-    a, b = map(int, input().split())
+    a,b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
