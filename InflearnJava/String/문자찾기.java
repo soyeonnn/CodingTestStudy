63f2ca916c4c99c0b1c54c32c6908044956693a6
soyeonnn <no02031@naver.com>
2022-07-31 21:07:38 +0900

    inflearnJava 문자찾기

---
+++ InflearnJava/String/문자찾기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+  
+public class Main {
+  public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
+    int input1 = in.nextInt();
+    int input2 = in.nextInt();
+    System.out.println(input1 + input2);
+    return ;
+  }
+}
