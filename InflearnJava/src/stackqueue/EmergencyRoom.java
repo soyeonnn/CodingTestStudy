862c195445a8a8c1e265e306d0288d2fecbefb12
soyeonnn <no02031@naver.com>
2022-08-14 20:30:25 +0900

    stack queue 완료

--- InflearnJava/src/stackqueue/EmergencyRoom.java
+++ InflearnJava/src/stackqueue/EmergencyRoom.java
@@ -1,12 +1,30 @@
 package stackqueue;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
+// 풀이X
 public class EmergencyRoom {
 
 	public int solution(int n, int m, int[] arr) {
-		int answer = 0;
-		
+		int answer = 0, patient = arr[m];
+		Queue<Integer> Q = new LinkedList<>();
+		for(int x : arr) Q.add(x);
+		while(!Q.isEmpty()) {
+			int popNum = Q.poll();
+			boolean isMax = true;
+			for(int i=0; i<Q.size(); i++) {     
+				if(Q.peek() < popNum) Q.add(Q.poll());
+				else {
+					Q.add(popNum);
+					isMax = false;
+					break;
+				}				
+			}
+			if(isMax && (patient == popNum)) return answer+1;
+			else if(isMax && (patient != popNum)) answer++;
+		}		
 		return answer;
 	}
 	
