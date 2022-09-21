143035015abdfbe291418774739e94acd3d90b26
soyeonnn <no02031@naver.com>
2022-09-21 10:55:12 +0900

    입출력과 사칙연산 완료

---
+++ BaekjoonJava/src/ioarithmeticoperations/S10171.java
@@ -0,0 +1,11 @@
+package ioarithmeticoperations;
+
+public class S10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
