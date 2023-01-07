164f9aae6a817391ed902b2bfa990ea00754e753
soyeonnn <no02031@naver.com>
2023-01-07 16:05:25 +0900

    씨름선수 solution

---
+++ InflearnJavaIJ/src/dfsbfs/WrestlerSolution.java
@@ -0,0 +1,49 @@
+package dfsbfs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class Body implements Comparable<Body> {
+    public int h, w;
+
+    public Body(int h, int w) {
+        this.h = h;
+        this.w = w;
+    }
+
+    @Override
+    public int compareTo(Body o) {
+        return o.h-this.h;
+    }
+}
+
+public class WrestlerSolution {
+
+    public int solution(ArrayList<Body> arr, int n) {
+        int cnt = 0;
+        Collections.sort(arr);
+        int max = Integer.MIN_VALUE;
+        for(Body ob : arr) {
+            if(ob.w > max) {
+                max = ob.w;
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        WrestlerSolution T = new WrestlerSolution();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Body> arr = new ArrayList<>();
+        for(int i=0; i<n; i++) {
+            int h = sc.nextInt();
+            int w = sc.nextInt();
+            arr.add(new Body(h, w));
+        }
+        System.out.println(T.solution(arr, n));
+
+    }
+}
