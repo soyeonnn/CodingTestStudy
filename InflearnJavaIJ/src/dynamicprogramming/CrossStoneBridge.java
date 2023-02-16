21f6564f4e803e73c038e7d165e1a1875440fd81
soyeonnn <no02031@naver.com>
2023-02-16 15:03:12 +0900

    돌다리 건너기

---
+++ InflearnJavaIJ/src/dynamicprogramming/CrossStoneBridge.java
@@ -0,0 +1,25 @@
+package dynamicprogramming;
+
+import java.util.Scanner;
+
+public class CrossStoneBridge {
+
+    static int[] dy;
+
+    public int solution(int n) {
+        dy[1] = 1;
+        dy[2] = 2;
+        for(int i=3; i<=n+1; i++) {
+            dy[i] = dy[i-1] + dy[i-2];
+        }
+        return dy[n+1];
+    }
+
+    public static void main(String[] args) {
+        CrossStoneBridge T = new CrossStoneBridge();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        dy = new int[n + 2];
+        System.out.println(T.solution(n));
+    }
+}
